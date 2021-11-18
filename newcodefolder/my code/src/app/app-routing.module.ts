3dfa802adaee6dec7b2ea3526f7a9d4ab7678c91
drilonu <86578785+drilonu@users.noreply.github.com>
2021-11-18 14:55:54 +0000

    Films component
    Import and route path add

--- newcodefolder/my code/src/app/app-routing.module.ts
+++ newcodefolder/my code/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { FilmsComponent } from './films/films.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'films', component: FilmsComponent },
   { path: 'heroes', component: HeroesComponent }
 ];
 
