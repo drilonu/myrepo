bea386d25c1d581a62934fb6e1a5c2c8bcae11f4
drilonu <86578785+drilonu@users.noreply.github.com>
2021-11-22 14:01:53 +0000

    number 7
    e,f, g

--- newcodefolder/my code/src/app/films/films.component.ts
+++ newcodefolder/my code/src/app/films/films.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import filmJSON from '../data/example_data1.json';
+
+interface film {
+  Year: string | null;
+  Length: number | null;
+  Title: string | null;
+  Subject: string | null;
+  Actor: string | null;
+  Director: string | null;
+  Popularity: number | null;
+  Awards: string | null;
+}
+
 @Component({
   selector: 'app-films',
   templateUrl: './films.component.html',
